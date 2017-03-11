078d20083aad9acf7be2b1061cabc60e8eed4e83
devMatthew <matthewhessin@gmail.com>
2017-03-11 17:09:19 -0500

    Update Command.java

--- org/bukkit/command/Command.java
+++ org/bukkit/command/Command.java
@@ -154,7 +154,7 @@ public abstract class Command {
         }
 
         if (permissionMessage == null) {
-            target.sendMessage(ChatColor.RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
+            target.sendMessage(ChatColor.RED + "This command either does not exist or you do not have permission!");
         } else if (permissionMessage.length() != 0) {
             for (String line : permissionMessage.replace("<permission>", permission).split("\n")) {
                 target.sendMessage(line);
